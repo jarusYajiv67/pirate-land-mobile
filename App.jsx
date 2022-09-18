65be73a98f7fb58f1b44cc5dfb182d3490efecd7
Suraj Vijay <99774088+jarusYajiv67@users.noreply.github.com>
2022-09-18 22:15:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+    </>
+  );
+}
